197fa0c8deb40d63be0976566a9e0d1d33cb9862
Martin Schmeißer <martinschmeisser18@googlemail.com>
2023-01-27 13:53:54 +0100

    Cleaning up repo

---
+++ 13_KNN_PlacingDP/Assets/Scripts/MyQRCodeManager.cs
@@ -0,0 +1,20 @@
+﻿using QRTracking;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyQRCodeManager : MonoBehaviour
+{
+    public QRCodesManager qRCodesManager;
+
+    public void StartScan()
+    {
+        // start QR tracking with the press of a button
+        qRCodesManager.StartQRTracking();
+    }
+    public void StopScan()
+    {
+        // Stop the tracking with the press of a button
+        qRCodesManager.StopQRTracking();
+    }
+}
