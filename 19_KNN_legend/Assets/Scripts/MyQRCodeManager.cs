97f672ba0496771b265228eb9506380ea1fdc738
Martin Schmeißer <martinschmeisser18@googlemail.com>
2023-02-03 18:19:31 +0100

    Added legend and infotext

---
+++ 19_KNN_legend/Assets/Scripts/MyQRCodeManager.cs
@@ -0,0 +1,23 @@
+﻿using QRTracking;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyQRCodeManager : MonoBehaviour
+{
+    public QRCodesManager qRCodesManager;
+    public TextMesh statusText;
+
+    public void StartScan()
+    {
+        // start QR tracking with the press of a button
+        qRCodesManager.StartQRTracking();
+        statusText.text = "Started QRCode Tracking";
+    }
+    public void StopScan()
+    {
+        // Stop the tracking with the press of a button
+        qRCodesManager.StopQRTracking();
+        statusText.text = "Stopped QRCode Tracking";
+    }
+}
